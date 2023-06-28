c1fefbb4cb5bec6f805cd59777ad04dd7e961390
Eliahdzc <eliamiriamhernandezcolin@gmail.com>
2023-06-27 22:47:06 -0600

    Cambiando las imagenes a public

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,19 +1,11 @@
-import {useState} from 'react'
 import "../Item/Item.css"
 import {Link} from 'react-router-dom'
 
 const Item = ({producto}) => {
 
-    const [image, setimage] = useState(undefined)
-
-    // Se importa el modulo con la imagen.
-    import(producto.imagen).then((img) => {
-        setimage(img.default)
-    })
-
     return (
         <div className="producto">
-            <img src={image} alt={producto.titulo} />
+            <img src={producto.imagen} alt={producto.titulo} />
             <div className="producto">
                 <h4>{producto.titulo}</h4>
                 <p>Categoria: {producto.categoria}</p>
