23523edaafd510b24e65b0ac29b7281476d408e3
Eliahdzc <eliamiriamhernandezcolin@gmail.com>
2023-06-27 22:30:51 -0600

    Agregando import dinamico para imagenes

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,18 +1,30 @@
+import {useState} from 'react'
 import "../Item/Item.css"
 import {Link} from 'react-router-dom'
 
-const Item = ( {producto}) => {
+const Item = ({producto}) => {
+
+    const [image, setimage] = useState(undefined)
+
+    // Se importa el modulo con la imagen.
+    import(producto.imagen).then((img) => {
+        setimage(img.default)
+    })
+
     return (
         <div className="producto">
-            <img src={producto.imagen} alt={producto.titulo} />
+            <img src={image} alt={producto.titulo} />
             <div className="producto">
                 <h4>{producto.titulo}</h4>
                 <p>Categoria: {producto.categoria}</p>
                 <p>Precio: ${producto.precio}</p>
-                <button className="boton-detalles"><Link to={`/productos/${producto.categoria}/${producto.id}`}>Descripcion</Link></button>
+                <button className="boton-detalles">
+                    <Link to={`/productos/${producto.categoria}/${producto.id}`}>
+                        Descripcion
+                    </Link>
+                </button>
 
             </div>
-
         </div>
     )
 }
